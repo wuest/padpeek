e4613a438d905ca2d98a5be72fd1ef6745d76dc5
Tina Wuest <tina@wuest.me>
2021-07-30 16:04:22 +0300

    Incremental progress, pushing for collab

--- pico/padpeek.c
+++ pico/padpeek.c
@@ -15,26 +15,26 @@
 #define CLOCK_SM 1
 #define DATA_SM 2
 
+volatile uint data_offset;
+
 void pio_pulse_irh()
 {
 	// IRQ 0 => latch
 	if(pio0_hw->irq & 1)
 	{
-		pio_sm_exec(pio0, DATA_SM, 0x0000 | 0x01);
-		pio0_hw->irq ^= 1;
+		pio_sm_exec(pio0, DATA_SM, 0x0000 | (data_offset + 0x01));
 	}
 	// IRQ 1 => clock
 	else if(pio0_hw->irq & 2)
 	{
-		pio_sm_exec(pio0, DATA_SM, 0x0000 | 0x04);
-		pio0_hw->irq ^= 2;
+		pio_sm_exec(pio0, DATA_SM, 0x0000 | (data_offset + 0x04));
 	}
 }
 
 int main()
 {
 	uint32_t frame;
-	uint latch_offset, clock_offset, data_offset;
+	uint latch_offset, clock_offset;
 
 	PIO pio = pio0;
 
@@ -49,8 +49,10 @@ int main()
 	data_program_init(pio, DATA_SM, data_offset, INPUT_DATA_PIN);
 
 	irq_set_exclusive_handler(PIO0_IRQ_0, pio_pulse_irh);
+	irq_set_exclusive_handler(PIO0_IRQ_1, pio_pulse_irh);
 	irq_set_enabled(PIO0_IRQ_0, true);
-	pio0_hw->inte0 = PIO_IRQ0_INTE_SM0_BITS | PIO_IRQ0_INTE_SM1_BITS;
+	irq_set_enabled(PIO0_IRQ_1, true);
+	pio0_hw->inte0 = PIO_IRQ0_INTE_SM0_BITS | PIO_IRQ0_INTE_SM1_BITS | PIO_IRQ0_INTE_SM2_BITS;
 
 	while(true)
 	{
