250a94e79389cb5c56eae473bf27b6337bd78ffe
Tina Wuest <tina@wuest.me>
2021-07-31 04:23:11 +0300

    Iteration, not quite there

--- pico/padpeek.c
+++ pico/padpeek.c
@@ -17,43 +17,44 @@
 
 volatile uint data_offset;
 
-void pio_pulse_irh()
+void __not_in_flash_func(pio_latch_irh)()
 {
-	// IRQ 0 => latch
-	if(pio0_hw->irq & 1)
-	{
-		pio_sm_exec(pio0, DATA_SM, 0x0000 | (data_offset + 0x01));
-	}
-	// IRQ 1 => clock
-	else if(pio0_hw->irq & 2)
-	{
-		pio_sm_exec(pio0, DATA_SM, 0x0000 | (data_offset + 0x04));
-	}
+	pio_sm_exec(pio0, DATA_SM, 0x0000 | (data_offset + 0x01));
+}
+
+void __not_in_flash_func(pio_clock_irh)()
+{
+	pio_sm_exec(pio0, DATA_SM, 0x0000 | (data_offset + 0x04));
 }
 
 int main()
 {
-	uint32_t frame;
+	uint16_t frame;
 	uint latch_offset, clock_offset;
 
 	PIO pio = pio0;
 
 	stdio_init_all();
 
-	latch_offset = pio_add_program(pio, &pulse_program);
-	clock_offset = pio_add_program(pio, &pulse_program);
+	latch_offset = pio_add_program(pio, &latch_program);
+	clock_offset = pio_add_program(pio, &clock_program);
 	data_offset = pio_add_program(pio, &data_program);
 
-	pulse_program_init(pio, LATCH_SM, latch_offset, INPUT_LATCH_PIN);
-	pulse_program_init(pio, CLOCK_SM, clock_offset, INPUT_CLOCK_PIN);
+    irq_set_exclusive_handler(PIO0_IRQ_0, pio_latch_irh);
+    irq_set_enabled(PIO0_IRQ_0, true);
+    irq_set_exclusive_handler(PIO0_IRQ_1, pio_clock_irh);
+    irq_set_enabled(PIO0_IRQ_1, true);
+    pio_set_irq0_source_enabled(pio, pis_interrupt0, true);
+    pio_set_irq1_source_enabled(pio, pis_interrupt1, true);
+
+    hw_clear_bits(&pio->inte0, 1u << LATCH_SM);
+    hw_clear_bits(&pio->inte1, 1u << CLOCK_SM);
+    hw_clear_bits(&pio->inte1, 1u << DATA_SM);
+	pio->irq = 0x7;
+
 	data_program_init(pio, DATA_SM, data_offset, INPUT_DATA_PIN);
-
-	irq_set_exclusive_handler(PIO0_IRQ_0, pio_pulse_irh);
-	irq_set_exclusive_handler(PIO0_IRQ_1, pio_pulse_irh);
-	irq_set_enabled(PIO0_IRQ_0, true);
-	irq_set_enabled(PIO0_IRQ_1, true);
-	pio0_hw->inte0 = PIO_IRQ0_INTE_SM0_BITS | PIO_IRQ0_INTE_SM1_BITS | PIO_IRQ0_INTE_SM2_BITS;
-
+	latch_program_init(pio, LATCH_SM, latch_offset, INPUT_LATCH_PIN);
+	clock_program_init(pio, CLOCK_SM, clock_offset, INPUT_CLOCK_PIN);
 	while(true)
 	{
 		frame = pio_sm_get_blocking(pio, DATA_SM);
